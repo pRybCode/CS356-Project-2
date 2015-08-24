b0163b7a6badf057bbeb8ce284a48770282f6836
Peter Rybicki <prybicki1@verizon.net>
2015-08-23 21:28:32 -0700

    Changes as of 8/23

---
+++ FeedObserver.java
@@ -0,0 +1,10 @@
+import java.util.Observable;
+import java.util.Observer;
+
+public class FeedObserver implements Observer 
+{
+	public void update(Observable FollowableComp, Object obj) 
+	{
+		
+	}
+}
